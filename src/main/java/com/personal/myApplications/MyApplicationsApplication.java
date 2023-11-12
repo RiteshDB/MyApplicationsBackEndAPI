c03d604aa3a035a478362c60cf1c6d05ce0194a0
RiteshDB <riteshdb123@gmail.com>
2023-11-12 17:11:03 +0530

    This is the setup commit

---
+++ src/main/java/com/personal/myApplications/MyApplicationsApplication.java
@@ -0,0 +1,14 @@
+package com.personal.myApplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplicationsApplication.class, args);
+		System.out.println("Project is created successfully");
+	}
+
+}
---
+++ src/test/java/com/personal/myApplications/MyApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.myApplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
